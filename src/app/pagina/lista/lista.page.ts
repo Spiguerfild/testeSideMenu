ce7901c91b84caa1de970c598f4dc7f226cd980a
Spiguerfild <luique16@outlook.com>
2023-08-07 22:25:39 -0400

    update imgs

--- src/app/pagina/lista/lista.page.ts
+++ src/app/pagina/lista/lista.page.ts
@@ -7,6 +7,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListaPage implements OnInit {
 
+  img1 = "/assets/meliodasImg.png"
+  img2 = "/assets/harlequinImg.jpg"
+
+
   constructor() { }
 
   ngOnInit() {
