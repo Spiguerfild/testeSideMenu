7289348d2175f9a8eeeb30781cb11ab6280f38fe
Spiguerfild <luique16@outlook.com>
2023-08-09 22:37:20 -0400

    muitos componentes

--- src/app/pagina/lista/lista.page.ts
+++ src/app/pagina/lista/lista.page.ts
@@ -7,9 +7,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListaPage implements OnInit {
 
-  img1 = "/assets/meliodasImg.png"
-  img2 = "/assets/harlequinImg.jpg"
+  // nomeAtor1 = "Meliodas";
+  // avatarAtor1 = "/assets/meliodasImg.png";
 
+  // nomeAtor2 = "Harlequin";
+  // avatarAtor2 = "/assets/harlequinImg.jpg";
+
+  nome = "Meliodas";
+  caminho = "/assets/meliodasImg.png";
+  curtidas = '500';
+
+  elenco = [
+
+    {
+      nome: "Harlequin",
+      caminho: "/assets/harlequinImg.jpg",
+      curtidas: '50'
+    }
+  ]
 
   constructor() { }
 
